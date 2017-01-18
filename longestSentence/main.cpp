862d39d764d2d3cc42c899e95688b3e17f18469e
Bill Brubaker <brubaker.bill@gmail.com>
2017-01-17 18:27:26 -0600

    first commit

---
+++ longestSentence/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  longestSentence
+//
+//  Created by William Brubaker on 1/17/17.
+//  Copyright © 2017 William Brubaker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
